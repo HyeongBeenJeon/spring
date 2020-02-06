8538f27eaa4961b6f7591c814cd9c106e543b032
salvere0 <apporiate@naver.com>
2020-02-06 12:37:48 +0900

    수정1

--- testGetMessage/src/test/TestInsert.java
+++ testGetMessage/src/test/TestInsert.java
@@ -1,7 +1,19 @@
 package test;
 
+import springMybatis0204.mapper.DepartmentMapper;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/springContext.xml")
 public class TestInsert
 {
+	@Autowired
+	DepartmentMapper departmentMapper;
 	public void InsertTest()
 	{
 		Department department = new Department();
